7e8bb0873ebb4e6819e5549305261390bb89835f
DiegoMendoz <daygon2001@gmail.com>
2026-08-29 20:00:32 -0600

    Creacion de el AppDbContext y AppDbContext con DbSet de Tareas (SQLite)

---
+++ SerfinsaBackend/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SerfinsaBackend.Models;
+
+namespace SerfinsaBackend.Data
+
+    
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+            { }
+        public DbSet<Tarea> Tareas => Set<Tarea>();
+    }
+}
