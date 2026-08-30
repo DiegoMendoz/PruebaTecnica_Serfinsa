43e71a695b66d7db158573b17f1759224ca32007
DiegoMendoz <daygon2001@gmail.com>
2026-08-29 20:04:55 -0600

    creacion de dto de Tarea

---
+++ SerfinsaBackend/Dtos/TareaDto.cs
@@ -0,0 +1,15 @@
+﻿using SerfinsaBackend.Models;
+
+namespace SerfinsaBackend.Dtos
+{
+    public class TareaDto
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; } = string.Empty;
+        public string? Descripcion { get; set; }
+        public Prioridad Prioridad { get; set; }
+        public bool Completada { get; set; }
+        public DateTime FechaCreacion { get; set; }
+
+    }
+}
