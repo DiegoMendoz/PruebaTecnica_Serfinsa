42e3769dec9d08cde9d60779655e20cdbaefe292
DiegoMendoz <daygon2001@gmail.com>
2026-08-29 19:57:09 -0600

    creacion de modelo tarea con prioridad enum

---
+++ SerfinsaBackend/Models/Tarea.cs
@@ -0,0 +1,22 @@
+﻿namespace SerfinsaBackend.Models
+{
+    public class Tarea
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; } = string.Empty;
+        public string? Descripcion { get; set; }
+        public Prioridad Prioridad { get; set; } = Prioridad.Media;
+        public bool Completada { get; set; } = false;
+        public DateTime FechaCreacion { get; set; } = DateTime.UtcNow;
+    }
+
+        public enum Prioridad
+        {
+            Baja,
+            Media,
+            Alta
+        }
+      
+        
+    }
+
